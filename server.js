698dd9a21290d3c9d99877e2855833b2b559d153
reedoooo <readvogt@gmail.com>
2023-02-24 08:56:13 -0800

    fixed server

--- server.js
+++ server.js
@@ -1,28 +1,21 @@
-'use strict';
+"use strict";
 
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const getWeather = require("./modules/weather");
+const getMovies = require("./modules/movies");
 
 const app = express();
 
 app.use(cors());
-app.get('/weather', getWeather);
-app.get('/movies', getMovies);
+app.get("/weather", getWeather);
+app.get("/movies", getMovies);
 
-// const PORT = process.env.PORT || 8080;
-
-// const location = require('./modules/location');
-const getWeather = require('./modules/weather');
-const getMovies = require('./modules/movies');
-
-
-// app.use(express.static('public'));
-
-app.get('/', (request, response) => {
-  response.send('Your default endpoint is working');
+app.get("/", (request, response) => {
+  response.send("Your default endpoint is working");
 });
 
-app.listen(process.env.PORT, () =>
-  console.log(`Server up on ${process.env.PORT}`)
-);
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server up on ${PORT}`));
