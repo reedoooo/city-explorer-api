c0c657e71e76ac501ca566e388e048726b4cc010
reedoooo <readvogt@gmail.com>
2023-02-25 02:07:24 -0800

    final fr fr

--- server.js
+++ server.js
@@ -2,25 +2,28 @@
 
 require("dotenv").config();
 const express = require("express");
+const app = express();
+
 const cors = require("cors");
 
 const getWeather = require("./modules/weather");
 const getMovies = require("./modules/movies");
 
-const app = express();
 
 app.use(cors());
-app.use(function (req, res, next) {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://newestbranch--reedthahuman-city-explorer.netlify.app"
-  );
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+
+// app.use(function (req, res, next) {
+//   res.header(
+//     "Access-Control-Allow-Origin",
+//     "https://newestbranch--reedthahuman-city-explorer.netlify.app"
+//   );
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
+
 app.get("/weather", getWeather);
 app.get("/movies", getMovies);
 
