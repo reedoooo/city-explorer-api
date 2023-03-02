dbebe216f1bfbddbfd97cafb0defc2fe5a089632
reedoooo <readvogt@gmail.com>
2023-03-01 19:43:07 -0800

    ddd

--- server.js
+++ server.js
@@ -1,36 +1,32 @@
 "use strict";
 
 require("dotenv").config();
-const express = require("express");
-const app = express();
 
+const express = require("express");
 const cors = require("cors");
+const app = express();
+app.use(cors());
+const PORT = process.env.PORT || 3001;
 
 const getWeather = require("./modules/weather");
 const getMovies = require("./modules/movies");
 const getYelp = require("./modules/yelp");
 
-app.use(cors());
-
-// app.use(function (req, res, next) {
-//   res.header(
-//     "Access-Control-Allow-Origin",
-//     "https://newestbranch--reedthahuman-city-explorer.netlify.app"
-//   );
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
+app.get("/", (req, res) => {
+  res.status(200).send("Hello World");
+});
 
 app.get("/weather", getWeather);
 app.get("/movies", getMovies);
 app.get("/yelp", getYelp);
 
-app.get("/", (request, response) => {
-  response.send("Your default endpoint is working");
+app.use("*", (req, res) => {
+  res.status(404).send("Not Found");
 });
 
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log(`Server up on ${PORT}`));
+app.use((error, req, res, next) => {
+  console.error(error);
+  res.status(500).send("Server Error");
+});
+
+app.listen(PORT, () => console.log(`Server up on port ${PORT}`));
