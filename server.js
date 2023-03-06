4dfed95aeb19e565da7d6bdc80153ce0563f1224
reedoooo <readvogt@gmail.com>
2023-03-05 22:12:52 -0700

    add cache

--- server.js
+++ server.js
@@ -12,13 +12,13 @@ const getWeather = require("./modules/weather");
 const getMovies = require("./modules/movies");
 const getYelp = require("./modules/yelp");
 
-app.get("/", (req, res) => {
-  res.status(200).send("Hello World");
-});
-
 app.get("/weather", getWeather);
 app.get("/movies", getMovies);
 app.get("/yelp", getYelp);
+
+app.get("/", (req, res) => {
+  res.status(200).send("Hello World");
+});
 
 app.use("*", (req, res) => {
   res.status(404).send("Not Found");
